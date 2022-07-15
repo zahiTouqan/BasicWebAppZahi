90cd635139cbdfbbf419869d400d7dd83395a8e6
Zahi Touqan <zahi.touqan@tum.de>
2022-07-15 11:06:22 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,7 +2,10 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class QueryProcessor {
@@ -15,17 +18,27 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "zahi";
-        } else if (query.contains("plus")){ // TODO extend the programm here
+        } else if (query.contains("plus")){ // TODO extend the program here
             String[] queryParts = query.split(":");
             String[] all = queryParts[1].split("%20");
             int result = Integer.parseInt(all[2]) + Integer.parseInt(all[4]);
             return String.valueOf(result);
         } else if (query.contains("largest")){
             String[] queryParts = query.split(":");
-            String[] all = queryParts[1].split("%20");
-            int[] nums = new int[all.length];
-            for (int i = 0; i < all.length; i++) {
-                nums[i] = Integer.parseInt(all[i]);
+            String[] all = queryParts[2].split("%20");
+            List<String> arr = Arrays.asList(all);
+            List<Integer> toRemove = new ArrayList<>();
+            for (int i = 0; i < arr.size(); i++) {
+                if (Objects.equals(arr.get(i), "")) {
+                    toRemove.add(i);
+                } else if (arr.get(i).contains(",")) {
+                    arr.set(i, arr.get(i).replaceAll("[^a-zA-Z0-9]",""));
+                }
+            }
+
+            int[] nums = new int[arr.size() - 1];
+            for (int i = 1; i < arr.size(); i++) {
+                nums[i - 1] = Integer.parseInt(all[i]);
             }
             Arrays.sort(nums);
             return String.valueOf(nums[nums.length - 1]);
